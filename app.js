60786e7ddafe12a42db6db06b566091669d8ebc0
Anna Anderwald <anna.v.anderwald@gmail.com>
2021-06-30 17:37:21 -0400

    excercise is done, I think

--- app.js
+++ app.js
@@ -2,3 +2,33 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+let feedback = document.getElementById("feedback");
+let form = document.getElementById("form");
+let search = document.getElementById ("giphSearch");
+let img = document.getElementById("gif");
+
+
+const GIPHY_KEY = "McibqQ4RZDrINwqGsgEMRM8l8vWwtAgf";        //this is my giphy key this is put here and will be plugged in the fetch
+
+form.addEventListener("submit", fetchGiph);
+
+function fetchGiph(event) {
+    event.preventDefault();                                   //prevent the function default. you need this
+    
+    fetch(
+        `https://api.giphy.com/v1/gifs/translate?api_key=${GIPHY_KEY}&s=${search.value}`
+    )
+    .then((data) => data.json())
+    .then((parsedData) => {
+        img.src = parsedData.data.images.original.url;
+        img.alt= parsedData.data.title;
+    })
+    .catch((error) => {
+        console.log(error);
+        feedback.textContent = "I'm not working today";
+    
+    });  
+}
+
+
+
